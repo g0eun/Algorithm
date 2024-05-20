b863faaab15b7218f6eb5f257954f945b0df73ac
g0eun <ge.lee@wips.co.kr>
2024-05-20 16:41:50 +0900

    [workflow] README 스타일 변경

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -185,10 +185,9 @@ def main():
 
 
     # 배포 정보 추가
-    release_info = '${\\textsf{\color{lightgrey}' + f"Latest Release : {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}" + "}}$"
     context += f"""
 ##
-{release_info}"""
+Latest Release : {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}"""
 
 
     # 작성 파일 저장
